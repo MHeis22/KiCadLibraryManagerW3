e379f178414006a3f1648edb90b7ee1e6dfe4fa0
ManuHeiskanen <manuheiskanen@proton.me>
2026-04-20 21:10:28 +0300

    macOS stuff

--- autostart_darwin.go
+++ autostart_darwin.go
@@ -3,7 +3,16 @@
 
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+)
 
 //go:embed build/appicon.png
 var trayIcon []byte
+
+// ToggleAutoStart must be defined here for macOS so Wails can generate the binding.
+func (a *App) ToggleAutoStart(enable bool) error {
+	fmt.Println("--> AutoStart toggle is not yet implemented for macOS")
+	return nil
+}
